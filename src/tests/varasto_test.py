21d3fa52258f56e9940d7cd7ac007d3d67a140a6
Vesa <vesainwonderland@gmail.com>
2021-11-08 17:01:55 +0200

    1.10.1

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -66,5 +66,7 @@ class TestVarasto(unittest.TestCase):
         self.assertAlmostEqual(self.varasto.saldo, 0)
     
     def test_varaston_voi_tulostaa(self): 
+        self.assertTrue(False)
+
         self.varasto.lisaa_varastoon(5)
         self.assertEqual(str(self.varasto), "saldo = 5, vielä tilaa 5")
